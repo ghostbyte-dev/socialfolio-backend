d37b42656f967997bf0e42c0736a138c52d99a19
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-02-18 18:20:09 +0100

    fix wrong fields

--- services/explore.service.ts
+++ services/explore.service.ts
@@ -7,8 +7,6 @@ export class ExploreService {
     static async getExploreProfiles(): Promise<IExploreProfile[]> {
         const profiles = await User.find().limit(20);
 
-        
-
         if (!profiles) {
             throw new HttpError(404, "No profiles found");
         }
@@ -16,8 +14,8 @@ export class ExploreService {
         const exploreProfiles: IExploreProfile[] = profiles.map((user) => ({
             id: user._id.toString(),
             username: user.username,
-            avatar: user.username || "",
-            bio: user.username || "",
+            avatar: user.avatarUrl || "",
+            description: user.description || "",
         }));
 
         return exploreProfiles;
