76aa47b2205e213f5070e087ca7127c3c69f6a8c
Hiebeler <emanuel.hiebeler@gmail.com>
2026-02-04 17:36:27 +0100

    add logs

--- services/view.service.ts
+++ services/view.service.ts
@@ -11,8 +11,8 @@ export class ViewService {
       NX: true,
       EX: 86400,
     });
-    
-    console.log("new view", isNewView);
+
+    console.log("new view", isNewView, ip);
     if (isNewView === "OK") {
         console.log("save view")
       await View.create({
