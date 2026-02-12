7cff956807bada196c7a664efeca3bd01b2c6ded
Hiebeler <emanuel.hiebeler@gmail.com>
2026-02-12 13:08:31 +0100

    ignore useragent node

--- utils/isBot.ts
+++ utils/isBot.ts
@@ -1,8 +1,8 @@
 import { RouterContext } from "@oak/oak/router";
 import { GET_BY_USERNAME_ROUTE } from "../routes/user.routes.ts";
 import { botRequestCounter } from "../main.ts";
-import { isBot as uaParserIsBot} from 'ua-parser-js/bot-detection';
-import { UAParser} from 'ua-parser-js';
+import { isBot as uaParserIsBot } from "ua-parser-js/bot-detection";
+import { UAParser } from "ua-parser-js";
 
 export const isBot = (
   context: RouterContext<typeof GET_BY_USERNAME_ROUTE>,
@@ -12,19 +12,23 @@ export const isBot = (
     botRequestCounter.add(1, { reason: "no_user_agent" });
     return true;
   }
+  if (userAgent === "node") {
+    botRequestCounter.add(1, { agent: "node" });
+
+    return false;
+  }
 
   const isBot = uaParserIsBot(userAgent);
 
   if (isBot) {
-    const parser = new UAParser(userAgent)
+    const parser = new UAParser(userAgent);
     const browserResult = parser.getBrowser();
-    
 
     const botName = browserResult.name || "Generic Bot";
-    
-    botRequestCounter.add(1, { 
+
+    botRequestCounter.add(1, {
       agent: botName,
-      type: "regex_match" 
+      type: "regex_match",
     });
   }
 
