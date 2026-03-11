ada03df770e2ad40f1a96c457fd3598df666c6df
Emanuel Hiebeler <e12519646@student.tuwien.ac.at>
2026-03-11 20:03:44 +0100

    remove logs

--- services/user.service.ts
+++ services/user.service.ts
@@ -38,7 +38,6 @@ export class UserService {
     }
 
     if (profile.status == Status.Disabled && profileId != jwtUserId) {
-      console.log("disabled: " + profileId + " , " + jwtUserId);
       throw new HttpError(400, "This Profile is disabled");
     }
 
